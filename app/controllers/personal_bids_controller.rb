46fa0557ab89b26f3a78349f27b486a28048c0dd
Agnieszka <agnieszka@agnieszka-laptop.(none)>
2010-04-11 13:59:57 +0200

    Modyfikacja wyświetlania wyników wyszukiwania (wyniki na szerokość całej strony, dodany nr aukcji). Nr aukcji dodany jest również do listy utworzonych aukcji i listy licytacji, w których bierze się udział jako licytujący.

--- app/controllers/personal_bids_controller.rb
+++ app/controllers/personal_bids_controller.rb
@@ -13,10 +13,12 @@ class PersonalBidsController < ApplicationController
     end
 #    @auctions = Bid.find_all_by_user_id current_user
 
-    @auctions = Bid.find_by_sql "SELECT * FROM auctions a
-                                 INNER JOIN bids b
-                                 ON a.id=b.auction_id
-                                 WHERE b.user_id="+current_user.id.to_s
+    @auctions = Bid.find_by_sql "SELECT b.auction_id, a.auctionable_type, a.current_price, MAX(b.offered_price) AS \"offered_price\", a.auctionable_id FROM auctions a
+INNER JOIN bids b
+ON a.id=b.auction_id
+WHERE b.user_id="+current_user.id.to_s+"
+GROUP BY b.auction_id, a.auctionable_type, a.current_price, a.auctionable_id
+ORDER BY b.auction_id";
 
    
   #  @auctions = Auction.user_id_equals(current_user.id).all
