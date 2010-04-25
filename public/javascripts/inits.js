324516a6b905aedeb7a8e424e21c03da57fa8caa
Agnieszka <agnieszka@agnieszka-laptop.(none)>
2010-04-25 19:18:23 +0200

    wizard: wybór typu aukcji w jquery, formatowanie dla edycji aukcji, poprawka css dla wyszukiwania, usuniete niepotrzebne pliki

--- public/javascripts/inits.js
+++ public/javascripts/inits.js
@@ -2,23 +2,6 @@
                             mode:"textareas",
                             editor_selector : "tiny_mce"
                     });
-                    function setAuction(frm) /* wyłącz pola kup-teraz */
-                    {
-						 frm.ch_buyNow.checked=false;  /* wyłącz checkbox*/
-                         frm.minimal_price.disabled=false;
-                         frm.minimal_bidding_difference.disabled=false;
-                         frm.buy_now_price.disabled=true;
-                    }
-
-                    function setBuyNow(frm)  /*wyłącz pola licytacji*/
-                    {
-                          frm.ch_auction.checked=false; /* wyłącz checkbox*/
-						  frm.site_link_auction_attributes_buy_now_price.disabled=false;
-                          frm.minimal_price.disabled=true;
-                          frm.minimal_price.value=0.0;
-                          frm.minimal_bidding_difference.disabled=true;
-                    }
-
                     $(function() {
                         $("#datepicker_from").datepicker({
                               showOn: 'button',
@@ -32,12 +15,12 @@
                         });
                     });
 
+   
         $(document).ready(function()
         {
           $("#search_categories").fcbkcomplete({
-            firstselected: true,
-            cache: true,
-            filter_case: true,
-            filter_hide: true});
-          
+            cache: true});
+
+          setWizardCheckboxes();
+
         });
