97d78de03438efb28f34d9471c2de44d9414f2bd
Agnieszka <agnieszka@agnieszka-laptop.(none)>
2010-04-28 17:58:46 +0200

    porządki w folderze personal

--- app/controllers/personal_controller.rb
+++ app/controllers/personal_controller.rb
@@ -4,9 +4,9 @@ class PersonalController < ApplicationController
       deny :banned, :not_activated
     end
     rescue_from Acl9::AccessDenied, :with => :deny_user_access
-  
-  
-  def index
+
+
+  def created_auctions
     if !current_user
       flash[:notice] = "Musisz się zalogować, jak tu wszedłeś ?"
       redirect_to :root
@@ -24,6 +24,22 @@ class PersonalController < ApplicationController
     end
     bids = Bid.not_cancelled.user_id_equals(current_user.id).all.map{|x| x.auction_id}
     @auctions = Auction.id_in(bids)
+  end
+ def bids
+    if !current_user
+      flash[:notice] = "Musisz się zalogować, jak tu wszedłeś ?"
+      redirect_to :root
+      return
+    end
+#    @auctions = Bid.find_all_by_user_id current_user
+
+    @auctions = Bid.find_by_sql "SELECT b.auction_id, a.auctionable_type, a.current_price, MAX(b.offered_price) AS \"offered_price\", a.auctionable_id FROM auctions a
+    INNER JOIN bids b
+    ON a.id=b.auction_id
+    WHERE b.user_id="+current_user.id.to_s+"
+    GROUP BY b.auction_id, a.auctionable_type, a.current_price, a.auctionable_id
+    ORDER BY b.auction_id";
+      #  @auctions = Auction.user_id_equals(current_user.id).all
   end
 
   def observed
@@ -85,5 +101,7 @@ class PersonalController < ApplicationController
     #TODO Tu ma się pojawić redirect do powiadomienia o tym, że trzeba zaktywować
     return
   end
-  
+
+
+
 end
